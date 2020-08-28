da617cacff8bd52f83f7278ec321b453dc676f60
alvaro aguirre <alvaro.aguirre@phinxlab.com>
2020-08-28 18:07:52 -0300

    commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "./index.html",
+    }),
+  ],
+};
